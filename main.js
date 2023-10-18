23fc1c33930dac90ede9bdc67a9f8f4608963f63
guhmorelli <144248100+guhmorelli@users.noreply.github.com>
2023-10-18 11:15:04 -0300

    ..

---
+++ main.js
@@ -0,0 +1,24 @@
+// main.js
+
+let display = document.getElementById ("display");
+let currentlnput = " ";
+
+function appendToDisplay(value) {
+    currentlnput += value;
+    display.value = currentlnput;
+}
+
+function clearDisplay(){
+    currentlnput = " ";
+    display.value = " ";
+}
+
+function caculateResult() {
+    try {
+        currentlnput = eval (currentlnput);
+        display.value = currentlnput; 
+}
+
+
+}
+       

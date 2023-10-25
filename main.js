663e3051c5b4ae7e0808c08badf1dab391d9fa2f
guhmorelli <144248100+guhmorelli@users.noreply.github.com>
2023-10-25 10:51:57 -0300

    calculadora do morelli

--- main.js
+++ main.js
@@ -9,16 +9,16 @@ function appendToDisplay(value) {
 }
 
 function clearDisplay(){
-    currentlnput = " ";
-    display.value = " ";
+    currentlnput = "";
+    display.value = "";
 }
 
 function caculateResult() {
     try {
         currentlnput = eval (currentlnput);
         display.value = currentlnput; 
-}
-
-
+}catch(error){
+    display.value="Erro";
+ }
 }
        
